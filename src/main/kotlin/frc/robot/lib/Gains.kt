9f17ea0cb69f2c143cd14c6d7b61aa288346bc2b
AdarWa <adar.wasserman@gmail.com>
2025-07-19 09:16:53 +0300

    Add an extension function to convert `Gains` to `Slot0Configs`.

--- src/main/kotlin/frc/robot/lib/Gains.kt
+++ src/main/kotlin/frc/robot/lib/Gains.kt
@@ -1,5 +1,21 @@
 package frc.robot.lib
 
+import com.ctre.phoenix6.configs.Slot0Configs
+
+/**
+ * An extension function to convert a [Gains] type to the [Slot0Configs] that the motor uses.
+ */
+fun Gains.toSlotConfig() =
+    Slot0Configs().apply {
+        kP = this@toSlotConfig.kP
+        kI = this@toSlotConfig.kI
+        kD = this@toSlotConfig.kD
+        kA = this@toSlotConfig.kA
+        kS = this@toSlotConfig.kS
+        kV = this@toSlotConfig.kV
+        kG = this@toSlotConfig.kG
+    }
+
 data class Gains(
     val kP: Double = 0.0,
     val kI: Double = 0.0,
