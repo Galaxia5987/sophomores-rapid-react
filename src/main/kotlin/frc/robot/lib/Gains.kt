389cb1c9e9615d7e912c5030ba68ded51af74799
AdarWa <adar.wasserman@gmail.com>
2025-07-20 23:29:52 +0300

    Make `toSlotConfig` a regular method of `Gains`

--- src/main/kotlin/frc/robot/lib/Gains.kt
+++ src/main/kotlin/frc/robot/lib/Gains.kt
@@ -2,21 +2,6 @@ package frc.robot.lib
 
 import com.ctre.phoenix6.configs.Slot0Configs
 
-/**
- * An extension function to convert a [Gains] type to the [Slot0Configs] that
- * the motor uses.
- */
-fun Gains.toSlotConfig() =
-    Slot0Configs().apply {
-        kP = this@toSlotConfig.kP
-        kI = this@toSlotConfig.kI
-        kD = this@toSlotConfig.kD
-        kA = this@toSlotConfig.kA
-        kS = this@toSlotConfig.kS
-        kV = this@toSlotConfig.kV
-        kG = this@toSlotConfig.kG
-    }
-
 data class Gains(
     val kP: Double = 0.0,
     val kI: Double = 0.0,
@@ -25,4 +10,19 @@ data class Gains(
     val kV: Double = 0.0,
     val kA: Double = 0.0,
     val kG: Double = 0.0
-)
+) {
+    /**
+     * A function to convert a [Gains] type to the [Slot0Configs] that
+     * the motor uses.
+     */
+    fun toSlotConfig() =
+        Slot0Configs().apply {
+            kP = this@Gains.kP
+            kI = this@Gains.kI
+            kD = this@Gains.kD
+            kA = this@Gains.kA
+            kS = this@Gains.kS
+            kV = this@Gains.kV
+            kG = this@Gains.kG
+        }
+}
