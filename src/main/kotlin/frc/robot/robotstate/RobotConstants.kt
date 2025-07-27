7257cf006df0f4d2c1d6fef86a9a04098e83a39d
יהלי <mor.yahali1612@gmail.com>
2025-07-27 19:46:59 +0300

    Add the turretRotationToBasket

--- src/main/kotlin/frc/robot/robotstate/RobotConstants.kt
+++ src/main/kotlin/frc/robot/robotstate/RobotConstants.kt
@@ -3,6 +3,7 @@ package frc.robot.robotstate
 import frc.robot.drive
 import frc.robot.lib.extensions.distanceFromPoint
 import frc.robot.lib.extensions.m
+import frc.robot.lib.extensions.rotationFromPoint
 import frc.robot.lib.getPose2d
 
 val minDistanceFromBasket = 0.5.m
@@ -10,3 +11,4 @@ val maxDistanceFromBasket = 3.m
 val basketLocation = getPose2d()
 val robotDistanceFromBasket
     get() = drive.pose.distanceFromPoint(basketLocation.translation)
+val turretRotationToBasket get() = drive.pose.rotationFromPoint(basketLocation.translation)
