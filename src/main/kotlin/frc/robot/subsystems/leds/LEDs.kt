383a421e8f1d3955cf550261354a0fe57cd3187d
יהלי <mor.yahali1612@gmail.com>
2025-07-27 19:22:23 +0300

    Add led code

---
+++ src/main/kotlin/frc/robot/subsystems/leds/LEDs.kt
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.leds
+
+import edu.wpi.first.wpilibj.*
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+
+private val ledStrip =
+    AddressableLED(LED_STRIP_PORT).apply {
+        setLength(STRIP_LENGTH)
+        start()
+    }
+private val ledBuffer = AddressableLEDBuffer(STRIP_LENGTH)
+
+fun applyPattern(pattern: LEDPattern): Command = Commands.runOnce({
+    pattern.applyTo(ledBuffer)
+})
+
+fun StateColors.applyPattern() {
+    this.pattern.applyTo(ledBuffer)
+}
