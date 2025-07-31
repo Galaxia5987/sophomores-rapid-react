6090a764f10541f754cf28c3a9cd3ed99c4faac8
nogav <noga.veksler@gmail.com>
2025-07-31 17:47:50 +0300

    Change val INTAKE

--- src/main/kotlin/frc/robot/subsystems/roller/RollerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/roller/RollerConstants.kt
@@ -6,6 +6,6 @@ const val MOTOR_PORT = 0
 const val AUXILIARY_MOTOR_PORT = 1
 const val SENSOR_ID = 11
 
-val INTAKE = 1.0.volts
-val OUTTAKE = 1.0.volts
-val STOP = 0.0.volts
+val INTAKE = 1.volts
+val OUTTAKE = 1.volts
+val STOP = 0.volts
