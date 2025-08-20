0696cba112ef37d49ed19b63fd5ea97992ecc7b0
yalaka <mor.yahali1612@gmail.com>
2025-08-20 18:17:32 +0300

    Fixed some things in Wrist

--- src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -2,9 +2,11 @@ package frc.robot.subsystems.wrist
 
 import edu.wpi.first.units.measure.Angle
 import frc.robot.lib.extensions.deg
+import frc.robot.lib.extensions.degrees
 
+val POINTE_TOLERANCE = 1.0.degrees
+const val MOTOR_PORT = 2
 const val GEAR_RATIO = 1 / 69.82
-
 enum class Angles(degrees: Angle) {
     CLOSE(0.deg),
     OPEN(0.deg)
