53e7cb91c1e7d3c793d5d9db8707e7b76ec37c04
yahali <97111125+yakla@users.noreply.github.com>
2025-09-14 22:43:22 +0300

    Add LoggedNetworkGains

--- src/main/kotlin/frc/robot/lib/Gains.kt
+++ src/main/kotlin/frc/robot/lib/Gains.kt
@@ -1,6 +1,7 @@
 package frc.robot.lib
 
 import com.ctre.phoenix6.configs.Slot0Configs
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber
 
 data class Gains(
     val kP: Double = 0.0,
@@ -25,4 +26,37 @@ data class Gains(
             kV = this@Gains.kV
             kG = this@Gains.kG
         }
+
+
 }
+
+class LoggedNetworkGains(
+    name: String,
+    kP: Double = 0.0,
+    kI: Double = 0.0,
+    kD: Double = 0.0,
+    kS: Double = 0.0,
+    kV: Double = 0.0,
+    kA: Double = 0.0,
+    kG: Double = 0.0,
+    key: String = (Throwable().stackTrace[1]?.fileName?.substringBeforeLast('.') + ""),
+) {
+    val kP: LoggedNetworkNumber = LoggedNetworkNumber("/Tuning/${"$key/$name"}/kP", kP)
+    val kI: LoggedNetworkNumber = LoggedNetworkNumber("/Tuning/${"$key/$name"}/kI", kD)
+    val kD: LoggedNetworkNumber = LoggedNetworkNumber("/Tuning/${"$key/$name"}/kD", kI)
+    val kS: LoggedNetworkNumber = LoggedNetworkNumber("/Tuning/${"$key/$name"}/kS", kS)
+    val kV: LoggedNetworkNumber = LoggedNetworkNumber("/Tuning/${"$key/$name"}/kV", kV)
+    val kA: LoggedNetworkNumber = LoggedNetworkNumber("/Tuning/${"$key/$name"}/kA", kA)
+    val kG: LoggedNetworkNumber = LoggedNetworkNumber("/Tuning/${"$key/$name"}/kG", kG)
+
+    fun toSlotConfig() = Slot0Configs().apply {
+        kP = this@LoggedNetworkGains.kP.get()
+        kI = this@LoggedNetworkGains.kI.get()
+        kD = this@LoggedNetworkGains.kD.get()
+        kA = this@LoggedNetworkGains.kA.get()
+        kS = this@LoggedNetworkGains.kS.get()
+        kV = this@LoggedNetworkGains.kV.get()
+        kG = this@LoggedNetworkGains.kG.get()
+    }
+}
+
