ef5ae49422ee0d9b7c62b4e5e4485aa90f5a9617
nogav <noga.veksler@gmail.com>
2025-10-03 13:58:52 +0300

    flip joystick

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -62,8 +62,8 @@ object RobotContainer {
     private fun configureDefaultCommands() {
         drive.defaultCommand =
             DriveCommands.joystickDrive(
+                { driverController.leftY },
                 { driverController.leftX },
-                { -driverController.leftY },
                 { -driverController.rightX * 0.8 }
             )
         turret.defaultCommand = turret.setAngle { turretAngleToHub }
