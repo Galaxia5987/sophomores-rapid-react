401035e45a123f17236df35f58cb5058cdf48dda
yahali <97111125+yakla@users.noreply.github.com>
2025-10-09 19:39:08 +0300

    Update src/main/kotlin/frc/robot/lib/extensions/Measures.kt
    Co-authored-by: Dan Katzuv <31829093+katzuv@users.noreply.github.com>

--- src/main/kotlin/frc/robot/lib/extensions/Measures.kt
+++ src/main/kotlin/frc/robot/lib/extensions/Measures.kt
@@ -88,7 +88,7 @@ val rps
     get() = Units.RotationsPerSecond
 
 val rps_squared
-    get() = Units.RotationsPerSecond.per(Units.Second)
+    get() = Units.RotationsPerSecond / Second
 
 fun LinearVelocity.toAngular(
     diameter: Distance,
